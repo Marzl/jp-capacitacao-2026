e5609fc9fe5e2cf723c4d2f1f8819791abb424ad
marzl <zmarcelox@gmail.com>
2026-03-23 18:25:35 -0300

    feat: cria CategoriasRepository e valida duplicidade

---
+++ src/main/java/br/com/indra/marcelo_guedes/repository/CategoriasRepository.java
@@ -0,0 +1,11 @@
+package br.com.indra.marcelo_guedes.repository;
+
+import br.com.indra.marcelo_guedes.model.Categorias;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
+
+    boolean existsByNomeAndCategoriaPai(String nome, Categorias categoriaPai);
+}
