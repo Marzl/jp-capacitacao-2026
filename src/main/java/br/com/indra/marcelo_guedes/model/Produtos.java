b3e86f94de2ef430aa197c2cb7a3d40035038164
marzl <zmarcelox@gmail.com>
2026-03-27 08:42:40 -0300

    feat: implementa variaveis quantidadeEstoque e estoqueMinimo e padroniza model

--- src/main/java/br/com/indra/marcelo_guedes/model/Produtos.java
+++ src/main/java/br/com/indra/marcelo_guedes/model/Produtos.java
@@ -20,16 +20,23 @@ public class Produtos {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "nome", nullable = false)
     private String nome;
 
+    @Column(name = "descricao")
     private String descricao;
 
-    @Column(nullable = false)
+    @Column(name = "preco", nullable = false)
     private BigDecimal preco;
 
     @Column(name="codigo_barras")
     private String codigoBarras;
+
+    @Column(name="quantidade_estoque")
+    private Integer quantidadeEstoque = 0;
+
+    @Column(name="estoque_minimo")
+    private Integer estoqueMinimo = 0;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "categoria_id", nullable = false)
